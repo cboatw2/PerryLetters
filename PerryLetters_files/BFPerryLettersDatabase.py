899dd1757b664f77f942a71342f3ad8ab3426378
Candy Rae Boatwright <cboatw2@clemson.edu>
2025-09-07 06:49:15 -0400

    Database creation and expansion

---
+++ PerryLetters_files/BFPerryLettersDatabase.py
@@ -0,0 +1,60 @@
+import sqlite3
+
+conn = sqlite3.connect("BFPerryLetters.db")
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS people (
+    person_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE
+)
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS location (
+    location_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE,
+    latitude REAL,
+    longitude REAL
+)
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS letter (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    letter_number INTEGER,
+    date TEXT,
+    sent_from_location_id INTEGER,
+    sent_to_location_id INTEGER,
+    sender_id INTEGER,
+    recipient_id INTEGER,
+    file_path TEXT,
+    FOREIGN KEY(sent_from_location_id) REFERENCES location(location_id),
+    FOREIGN KEY(sent_to_location_id) REFERENCES location(location_id),
+    FOREIGN KEY(sender_id) REFERENCES people(person_id),
+    FOREIGN KEY(recipient_id) REFERENCES people(person_id)
+)
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS mentioned_location (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    letter_id INTEGER,
+    location_id INTEGER,
+    FOREIGN KEY(letter_id) REFERENCES letter(id),
+    FOREIGN KEY(location_id) REFERENCES location(location_id)
+)
+""")
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS mentioned_people (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    letter_id INTEGER,
+    person_id INTEGER,
+    FOREIGN KEY(letter_id) REFERENCES letter(id),
+    FOREIGN KEY(person_id) REFERENCES people(person_id)
+)
+""")
+
+conn.commit()
+conn.close()
