dcf0b3e0eebb7ca4511cff67ac25301adc65113c
Candy Rae Boatwright <cboatw2@clemson.edu>
2025-10-23 10:57:58 -0400

    Manual updates and fixes for the input module.

--- BFPerry_Letters_NER/BFPerry_Letters_NER.py
+++ BFPerry_Letters_NER/BFPerry_Letters_NER.py
@@ -1,17 +1,17 @@
 import spacy
 
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm") #load small English model
 
 input_file = "BFPerry_Letters_1863-1882.txt"
 
 with open(input_file, "r", encoding="utf-8", errors="ignore") as f:
-    text = f.read()
+    text = f.read() #read entire file into a string
 
-doc = nlp(text)
+doc = nlp(text) #process the text with spaCy NLP pipeline
 
 persons = set()
 places = set()
-for ent in doc.ents:
+for ent in doc.ents: #loops through spans of text identified as entities
     if ent.label_ == "PERSON":
         persons.add(ent.text)
     elif ent.label_ == "GPE":
